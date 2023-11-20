6162cac3f71e8c6bba83be9bddfdede11f1c2fd2
Fredrik Kivi <fredrik.kivi@ugli.se>
2023-11-21 00:37:41 +0100

    commit #2

--- icaco-core/src/main/java/io/icaco/core/App.java
+++ icaco-core/src/main/java/io/icaco/core/App.java
@@ -1,9 +1,16 @@
 package io.icaco.core;
 
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.eclipse.jgit.api.Git;
+
+import java.io.File;
+import java.io.IOException;
+
+public class App {
+    public static void main( String[] args ) throws Exception {
+        ;
+        File dir = new File(".");
+        System.out.println(dir.getAbsolutePath());
+        System.out.println( Git.open(dir).status().call().getModified());
+
     }
 }
