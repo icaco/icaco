5be9ce2ee68d447056aaad13a52071df69fda038
Fredrik Kivi <fredrik.kivi@ugli.se>
2024-10-04 20:05:49 +0200

    refactored

--- icaco-core/src/main/java/io/icaco/core/vcs/VcsType.java
+++ icaco-core/src/main/java/io/icaco/core/vcs/VcsType.java
@@ -1,15 +1,21 @@
 package io.icaco.core.vcs;
 
+import org.slf4j.Logger;
+
 import java.util.Optional;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 public enum VcsType {
     Git;
 
+    private static final Logger LOG = getLogger(VcsType.class);
+
     public static Optional<VcsType> findVcsType(String typeName) {
-        for (VcsType type: VcsType.values()) {
+        for (VcsType type: VcsType.values())
             if (type.name().equalsIgnoreCase(typeName))
                 return Optional.of(type);
-        }
+        LOG.warn("No type match for {} in {}", typeName, values());
         return Optional.empty();
     }
 }
