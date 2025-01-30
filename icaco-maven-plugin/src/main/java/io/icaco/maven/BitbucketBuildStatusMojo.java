47005fa637fa0607f9027e9b02a4462816a0c661
Fredrik <fredrik.kivi@arbetsformedlingen.se>
2025-01-30 17:18:53 +0100

    Tack för idag!

--- icaco-maven-plugin/src/main/java/io/icaco/maven/BitbucketBuildStatusMojo.java
+++ icaco-maven-plugin/src/main/java/io/icaco/maven/BitbucketBuildStatusMojo.java
@@ -3,10 +3,7 @@ package io.icaco.maven;
 import io.icaco.core.vcs.VcsLatestCommitCommand;
 import io.icaco.core.vcs.VcsRemoteUrlCommand;
 import io.icaco.core.vcs.VcsType;
-import org.apache.maven.execution.MavenExecutionResult;
-import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -14,6 +11,7 @@ import org.apache.maven.project.MavenProject;
 
 import javax.json.Json;
 import javax.json.JsonObject;
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -36,22 +34,16 @@ public class BitbucketBuildStatusMojo extends AbstractMojo {
     @Parameter(defaultValue = "${project}", required = true, readonly = true)
     MavenProject project;
 
-    @Parameter(defaultValue = "${session}", readonly = true, required = true)
-    MavenSession session;
-
-    @Parameter(defaultValue = "${mojoExecution}", readonly = true, required = true)
-    MojoExecution mojoExecution;
-
-    @Parameter(property = "vcsType", defaultValue = "git")
+    @Parameter(property = "vcsType", defaultValue = "git", required = true)
     String vcsType;
 
-    @Parameter(property = "bitbucket.token")
+    @Parameter(property = "bitbucket.token", required = true)
     String bitbucketToken;
 
-    @Parameter(property = "build.state", defaultValue = "INPROGRESS", required = true)
+    @Parameter(property = "build.state", required = true)
     String buildState;
 
-    @Parameter(property = "build.url")
+    @Parameter(property = "build.url", defaultValue = "https://bitbucket.org/", required = true)
     String buildUrl;
 
     final HttpClient httpClient = HttpClient.newBuilder()
@@ -72,40 +64,6 @@ public class BitbucketBuildStatusMojo extends AbstractMojo {
                 .filter(Matcher::find)
                 .map(m -> "https://" + m.group(1))
                 .orElseThrow(() -> new MojoExecutionException("Could not determine Bitbucket URL from git remote"));
-
-    }
-
-    String getBitbucketToken() throws MojoExecutionException {
-        if (bitbucketToken != null) {
-            return bitbucketToken;
-        }
-        String token = System.getenv("BITBUCKET_TOKEN");
-        if (token != null && !token.isEmpty()) {
-            return token;
-        }
-        throw new MojoExecutionException(
-                "No Bitbucket token found. Please set either bitbucket.token property " +
-                        "or BITBUCKET_TOKEN environment variable"
-        );
-    }
-
-    String generateBuildKey() {
-        return format("MAVEN-%s-%d",
-                project.getArtifactId(),
-                System.currentTimeMillis()
-        );
-    }
-
-    String getBuildName() {
-        return format("Maven Build - %s:%s",
-                project.getGroupId(),
-                project.getArtifactId()
-        );
-    }
-
-    boolean shouldFailOnError() {
-        MavenExecutionResult result = session.getResult();
-        return result == null || !result.hasExceptions();
     }
 
     @Override
@@ -118,26 +76,23 @@ public class BitbucketBuildStatusMojo extends AbstractMojo {
                     getBitbucketUrl(vcs, basePath),
                     latestCommit
             );
-            System.out.println(apiUrl);
-            if (true)
-                return;
+            getLog().info("API URL: " + apiUrl);
             JsonObject buildStatus = Json.createObjectBuilder()
                     .add("state", buildState)
-                    .add("key", generateBuildKey())
-                    .add("name", getBuildName())
-                    .add("url", buildUrl != null ? buildUrl : "")
+                    .add("key", latestCommit)
+                    .add("name", "Build " + latestCommit)
+                    .add("url", buildUrl)
                     .build();
-            System.out.println(buildStatus.toString());
-            if (true)
-                return;
+            String httpBody = buildStatus.toString();
+            getLog().info("Request Body: " + httpBody);
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(apiUrl))
                     .header("Content-Type", "application/json")
-                    .header("Authorization", "Bearer " + getBitbucketToken())
-                    .POST(HttpRequest.BodyPublishers.ofString(buildStatus.toString()))
+                    .header("Authorization", "Bearer " + bitbucketToken)
+                    .POST(HttpRequest.BodyPublishers.ofString(httpBody))
                     .build();
             HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
-            if (response.statusCode() >= 400) {
+            if (response.statusCode() != 204) {
                 throw new MojoExecutionException(
                         "Failed to update build status. Server returned: " +
                                 response.statusCode() + " " + response.body()
@@ -145,15 +100,12 @@ public class BitbucketBuildStatusMojo extends AbstractMojo {
             }
             getLog().info(format(
                     "Build status updated to %s for commit %s",
-                    buildStatus.getString("state"),
+                    buildState,
                     latestCommit
             ));
-        } catch (Exception e) {
-            if (shouldFailOnError()) {
-                throw new MojoExecutionException("Error updating build status", e);
-            } else {
-                getLog().error("Error updating build status", e);
-            }
+        } catch (IOException | InterruptedException e) {
+            throw new MojoExecutionException("Error updating build status", e);
         }
     }
+
 }
